1b0c1e593b087f9fce3a4fd53c3cb35b27a0f92b
Jobin Sunny <jobinsunny@gmail.com>
2016-04-04 04:09:00 +0000

    Annotated old classes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id                  :integer          not null, primary key
+#  first_name          :string
+#  last_name           :string
+#  bio                 :text
+#  education_level     :integer
+#  phone_number        :integer
+#  summary_of_projects :text
+#  skills              :text
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 class Profile < ActiveRecord::Base
-	validates :name, presence: true
+	validates :first_name, presence: true
 end
