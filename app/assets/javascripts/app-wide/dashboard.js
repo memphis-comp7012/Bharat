19f6d2df1ae16785c14e3d82683c3bbfc3a6f83a
Chandra Sekhar <cchllpll@memphis.edu>
2016-04-06 02:04:30 -0500

    Fixed JavaScript issue for Menu, Disabled jQuery MaskInput temporarily

--- app/assets/javascripts/app-wide/dashboard.js
+++ app/assets/javascripts/app-wide/dashboard.js
@@ -2,16 +2,6 @@ $(document).ready(function(){
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
-        $("footer").toggleClass(function(){
-            if ( !$( this ).hasClass( "toggled" ) ) {
-                $(this).removeClass('toggled');
-                return "non-toggled";
-            } else {
-                $(this).removeClass('non-toggled');
-                return "toggled";
-            }
-
-        });
         $( "#menu-toggle i" ).toggleClass(function() {
             if ( $( this ).hasClass( "fa-angle-double-left" ) ) {
                 $(this).removeClass('fa-angle-double-left');
