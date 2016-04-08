3b45343475a3cfe8c2e218dc855e5138c47a86b7
Jobin Sunny <jobinsunny@gmail.com>
2016-04-08 15:39:32 +0000

    Added better routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :projects
   resources :profile_research_fields
   resources :profiles
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   #resources :users
   root 'static_pages#home'
 
