73807a35c492f8c389ef1cea51bf8b2ab795b072
SreeLasya <vsreelasya@gmail.com>
2016-04-09 19:59:09 +0000

    Added validations

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,2 +1,9 @@
 class Contribution < ActiveRecord::Base
+	validates :score,
+	    numericality: { only_interger: true, greater_than_or_equal_to: -100, less_than_or_equal_to: 10000},
+         presence: true
+    validates :money_received,
+      numericality: { only_interger: true, greater_than_or_equal_to: 0,}
+      presence: true
+
 end
