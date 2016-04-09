ff8e29909f95197e819f7cc1345927ac3156ef3e
SreeLasya <vsreelasya@gmail.com>
2016-04-09 20:56:35 +0000

    fixed validations

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -3,7 +3,7 @@ class Contribution < ActiveRecord::Base
 	    numericality: { only_interger: true, greater_than_or_equal_to: -100, less_than_or_equal_to: 10000},
          presence: true
     validates :money_received,
-      numericality: { only_interger: true, greater_than_or_equal_to: 0,}
+      numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000},
       presence: true
 
 end
