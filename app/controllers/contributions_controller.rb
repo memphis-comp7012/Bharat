affee09a91103cc8cd56cbe380bd8d167b931996
Jobin Sunny <mvshisht@memphis.edu>
2016-05-01 02:25:57 +0000

    Mark projects complete

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -100,6 +100,13 @@ class ContributionsController < ApplicationController
     end
   end
 
+  def contribute
+    @project = Project.find(params[:project_id])
+    @project.status = 3
+    format.html { redirect_to @project, notice: 'Project was successfully completed.' }
+  end
+
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_contribution
