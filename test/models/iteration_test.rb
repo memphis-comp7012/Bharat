6a1615d8fdcd594484b1e8bac398ba7cb82860a0
Jobin Sunny <jobinsunny@gmail.com>
2016-05-05 12:50:57 +0000

    M2

--- test/models/iteration_test.rb
+++ test/models/iteration_test.rb
@@ -15,4 +15,21 @@ class IterationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "normal iteration should be valid" do
+    iter = iterations(:one)
+    assert iter.valid?
+  end
+
+  test "iteration with nil name should be invalid" do
+    iter = iterations(:one)
+    iter.name = nil
+    assert iter.invalid?
+  end
+
+  test "iteration with empty name should be invalid" do
+    iter = iterations(:one)
+    iter.name = ''
+    assert iter.invalid?
+  end
 end
